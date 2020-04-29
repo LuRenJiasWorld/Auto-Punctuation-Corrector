6fdd9c7df37d347f2bf76265a34b0e82532c2a44
Benjamin Chris <loli@lurenjia.in>
2020-04-29 22:54:27 +0800

    【Feat】合并过短行

--- postprocess.py
+++ postprocess.py
@@ -11,15 +11,30 @@ with open("./output.txt", "r") as file:
 
 for each_sentence in orig_text:
     final_text.append("")
-    print(each_sentence)
     current_sentence_list = each_sentence.split(" ")
-    while len(final_text[-1]) <= MAX_LINE:
-        if len(current_sentence_list) > 0:
-            final_text[-1] = final_text[-1] + " " + current_sentence_list.pop(0)
+    while True:
+        if len(final_text[-1]) <= MAX_LINE:
+            if len(current_sentence_list) > 0:
+                if final_text[-1] == "":
+                    final_text[-1] = current_sentence_list.pop(0)
+                else:
+                    final_text[-1] = final_text[-1] + " " + current_sentence_list.pop(0)
+            else:
+                final_text[-1] = final_text[-1] + "."
+                break
         else:
-            final_text[-1] = final_text[-1] + "."
-            break
+            final_text.append("")
+            continue
+
+final_final_text = []
+
+# 合并过短行（1/3长度）
+for each_line in final_text:
+    if len(each_line) < MAX_LINE / 3:
+        final_final_text[-1] = final_final_text[-1] + each_line
+    else:
+        final_final_text.append(each_line)
 
 with open("./output_postprocess.txt", "w") as file:
-    for each in final_text:
+    for each in final_final_text:
         file.write(each + "\n")
