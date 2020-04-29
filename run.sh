e7b300be251d36818b4a49d755ac7649691ce550
Benjamin Chris <loli@lurenjia.in>
2020-04-29 21:51:26 +0800

    【Perf】优化脚本

--- run.sh
+++ run.sh
@@ -10,22 +10,17 @@ fi
 rm -rf temp.txt output.txt output_post.txt
 
 echo "1. 编辑模式"
-read -s -n1 -p "编辑完成后输入:wq进入下一步，按任意键继续 ... "
+read -r -s -n1 -p "编辑完成后输入:wq进入下一步，按任意键继续 ... "
 vim temp.txt
 echo ""
 
 echo "2. 开始自动添加标点符号"
-cat temp.txt | ./venv/bin/python3 ./punctuator2/play_with_model.py model.pcl
+cat temp.txt | python3 ./punctuator2/play_with_model.py model.pcl
 
 echo "3. 开始润色文本"
-cat temp.txt | ./venv/bin/python3 ./punctuator2/convert_to_readable.py "temp.txt" "output.txt"
-
+cat temp.txt | python3 ./punctuator2/convert_to_readable.py "temp.txt" "output.txt"
 echo -e "文本处理完成，内容为:\n$(cat output.txt)"
 
 echo "4. 开始对文本进行后处理"
-cat output.txt | ./venv/bin/python3 ./postprocess.py
-
-echo -e "文本处理完成，内容为:\n$(cat output_post.txt)"
-
-# 清理临时文件
-rm -rf temp.txt output.txt output_post.txt
+cat output.txt | python3 ./postprocess.py
+echo -e "文本处理完成，内容为:\n$(cat output_postprocess.txt)"
